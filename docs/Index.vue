586824cab10b4e92dbbba7ea8d900c5639f5bfcd
huanggz <918031078@qq.com>
2022-10-11 16:00:21 +0800

    添加二维码

--- docs/Index.vue
+++ docs/Index.vue
@@ -1,5 +1,5 @@
 <template>
-    <img class="wechat-qrcode" src="/img/wechat-qrcode.jpg" alt="">
+    <img class="wechat-qrcode" src="/img/wechat-qrcode.jpg" alt="六码公众号">
 </template>
 
 <script setup>
